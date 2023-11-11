df826d6da08330c105a32a9a8c6ac3bc44df8afb
Yujun Won <124374862+Yujun-Won@users.noreply.github.com>
2023-11-11 16:29:37 +0900

    Title: 사칙연산 함수, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231111/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,30 @@
+def add(n,m):
+    return n + m
+
+def minus(n,m):
+    return n - m
+
+def multiply(n,m):
+    return n * m
+
+def divide(n,m):
+    return n // m
+
+def branch(n, m, o):
+    n, m = int(n), int(m)
+
+    if o == '+':
+        return add(n, m)
+    elif o == '-':
+        return minus(n, m)
+    elif o == '*':
+        return multiply(n, m)
+    elif o == '/':
+        return divide(n, m)
+    else:
+        return False
+
+
+a, o, c = input().split()
+
+print(f"{a} {o} {c} = {branch(a, c, o)}")
