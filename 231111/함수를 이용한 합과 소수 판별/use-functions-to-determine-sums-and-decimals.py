7fd44c47e0b8f964313e428786b119fbdb0f3776
Yujun Won <124374862+Yujun-Won@users.noreply.github.com>
2023-11-11 21:26:08 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 113ms, Memory: 25MB, Status: Passed - CodeTree

--- 231111/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
+++ 231111/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
@@ -1,22 +1,29 @@
-def is_prime(num):              # 소수 판별 함수
+# 소수 판별 함수
+def is_prime(num):
+    if num == 1:
+        return False
+
     for i in range(2, num):
         if num % i == 0:
             return False
     
     return True
 
-# a이상 b이하 수 중 소수 & 모든 자릿수 합이 짝수
+# 자리수의 합 짝수 여부 판단 함수
+def is_digit_sum_even(num):
+    digit_sum = (num // 100) + ((num // 10) % 10) + (num % 10)
+    if digit_sum % 2 == 0:
+        return True
+    
+    return False
+
+# 최종 판단 함수
 def judge_num(a, b):
     cnt = 0
     for i in range(a, b+1):
-        if is_prime(i):         # 소수일 경우
-            # 1. 한 자리 수
-            if len(str(i)) == 1 and i % 2 == 0:
-                cnt += 1
-            # 2. 두 자리 수
-            elif len(str(i)) == 2 and (i // 10 + i % 10) % 2 == 0:
-                cnt += 1
-    
+        if is_prime(i) and is_digit_sum_even(i):
+            cnt += 1
+            
     return cnt
 
 
