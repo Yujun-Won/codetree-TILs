47881ab567acfd613773447675d6f925ce365ed0
Yujun Won <124374862+Yujun-Won@users.noreply.github.com>
2023-11-12 11:30:40 +0900

    Title: DateTime to DateTime, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 231112/DateTime to DateTime/datetime-to-datetime.py
+++ 231112/DateTime to DateTime/datetime-to-datetime.py
@@ -1,16 +1,16 @@
-# def count_datetime(a, b, c):
+def count_datetime(a, b, c):
+    d = a - 11
+    h = b - 11
+    m = c - 11
+
+    if d == 0 and h == 0 and m == 0:        # 1. 같은 시간
+        mins = 0
+    elif d == 0 and (h < 0 or m < 0):       # 2. 이전 시간
+        mins = -1
+    else:
+        mins = d * 1440 + h * 60 + m
+
+    print(mins)
+
 a, b, c = map(int, input().split())
-
-d = a - 11
-h = b - 11
-m = c - 11
-
-mins = d * 1440 + h * 60 + m
-
-if a == 11 and h <= 11 and m < 11:
-    mins = -1
-
-
-
-print(mins)
-# print(count_datetime(a, b, c))
+count_datetime(a, b, c)
