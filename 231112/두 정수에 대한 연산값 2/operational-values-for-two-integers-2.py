68def02a1c7998625bfb24ce76aba534570ef96b
Yujun Won <124374862+Yujun-Won@users.noreply.github.com>
2023-11-12 16:36:25 +0900

    Title: 두 정수에 대한 연산값 2, Time: 137ms, Memory: 29MB, Status: Passed - CodeTree

---
+++ 231112/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -0,0 +1,15 @@
+def calculate(a, b):
+    if a > b:
+        a *= 2
+        b += 10
+    else:
+        a += 10
+        b *= 2
+    
+    return a, b
+
+
+a, b = map(int, input().split())
+
+a, b = calculate(a, b)
+print(a, b)
