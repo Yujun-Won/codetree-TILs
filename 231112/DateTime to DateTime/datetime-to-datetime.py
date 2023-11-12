6331e8b970fd2936bc65863601b17afdd76eb9f8
Yujun Won <124374862+Yujun-Won@users.noreply.github.com>
2023-11-12 11:19:49 +0900

    Title: DateTime to DateTime, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231112/DateTime to DateTime/datetime-to-datetime.py
+++ 231112/DateTime to DateTime/datetime-to-datetime.py
@@ -1,24 +1,15 @@
-def count_datetime(a, b, c):
-    # a: 일, b: 시, c: 분
-    mins = 0
-
-    # 1. 일
-    mins += (a - 11) * 1440
-
-    # 2. 시
-    if b < 11:
-        mins -= b * 60
-    else:
-        mins += (b - 11) * 60
-    
-    # 3. 분
-    if b < 11:
-        mins -= b
-    else:
-        mins += (b - 11)
-    
-    return mins + 1
-
-
+# def count_datetime(a, b, c):
 a, b, c = map(int, input().split())
-print(count_datetime(a, b, c))
+
+d = a - 11
+h = b - 11
+m = c - 11
+
+mins = d * 1440 + h * 60 + m
+
+
+
+
+
+print(mins)
+# print(count_datetime(a, b, c))
