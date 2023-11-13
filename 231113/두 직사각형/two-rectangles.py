0e6cf8b3460701f7e25123b43c427ab7bade7233
Yujun Won <124374862+Yujun-Won@users.noreply.github.com>
2023-11-13 09:32:54 +0900

    Title: 두 직사각형, Time: 174ms, Memory: 24MB, Status: Passed - CodeTree

--- 231113/두 직사각형/two-rectangles.py
+++ 231113/두 직사각형/two-rectangles.py
@@ -1,10 +1,10 @@
 def is_overlapped(x1, y1, x2, y2, a1, b1, a2, b2):
     if x2 < a1 or a2 < x1 or b2 < y1 or y2 < b1:
-        return "nonoverlapping"
+        return False
     else:
-        return "overlapping"
+        return True
 
 x1, y1, x2, y2 = map(int, input().split())
 a1, b1, a2, b2 = map(int, input().split())
 
-print(is_overlapped(x1, y1, x2, y2, a1, b1, a2, b2))
+print("overlapping" if is_overlapped(x1, y1, x2, y2, a1, b1, a2, b2) else "nonoverlapping")
