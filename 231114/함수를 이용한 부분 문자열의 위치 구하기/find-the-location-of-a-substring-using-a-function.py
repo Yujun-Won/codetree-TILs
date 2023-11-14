4be8537b1b89ba3cb49b26f25d0c47ceecc161dc
Yujun Won <124374862+Yujun-Won@users.noreply.github.com>
2023-11-14 22:06:42 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 149ms, Memory: 24MB, Status: Passed - CodeTree

--- 231114/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
+++ 231114/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -1,8 +1,29 @@
-def find_target_index():
-    return input_str.find(target_str)
+text = input()
+pattern = input()
 
 
-input_str = input()
-target_str = input()
+def is_substr(start_idx):
+    n, m = len(text), len(pattern)
+    
+    # 1. 만약 pattern을 매칭 시키기에 text 문자열 범위를 초과하는 경우
+    if start_idx + m - 1 >= n:
+        return False
 
-print(find_target_index())
+    # 2. text를 순회하며 pattern이 존재하는지 확인
+    for j in range(m):
+        if text[start_idx + j] != pattern[j]:
+            return False
+
+    return True
+
+
+def find_index():
+    n = len(text)
+    for i in range(n):
+        if is_substr(i):
+            return i
+
+    return -1
+
+
+print(find_index())
